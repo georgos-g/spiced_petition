c63f3910b5d634278cc2f129915f707c459d6e38
georgos_g <gg@dancingbear.de>
2020-07-19 18:42:16 +0200

    petition part5

--- db.js
+++ db.js
@@ -3,44 +3,112 @@ const { query } = require('express');
 dbUrl = process.env.DATABASE_URL || 'postgres:georgos:georgos@localhost:5432/petition'
 //const db = spicedPG("postgres:georgos:georgos@localhost:5432/petition");
 
-exports.saveSignature = (userID, signatureCode)=>{
+
+// exports.saveSignature = (userID, signatureCode)=>{
+//     return db.query(
+       
+//         'INSERT INTO signatures (user_id, signature_code) VALUES($1, $2) RETURNING id;',
+//         [userID, signatureCode]
+//         );
+// };
+
+
+exports.saveSignature = (userID, signatureCode) => {
     return db.query(
-        'INSERT INTO signatures (user_id, signature_code) VALUES($1, $2) RETURNING id;',
+        "INSERT INTO signatures (user_id, signature_code) VALUES($1, $2) RETURNING id;",
         [userID, signatureCode]
-        );
+    );
 };
 
-exports.saveUser = (firstname, lastname, email, pasword_hash) => {
+
+// exports.saveUser = (firstname, lastname, email, pasword_hash) => {
+//     return db.query(
+//         "INSERT INTO users (firstname, lastname, email, pasword_hash) VALUES($1, $2, $3, $4) RETURNING *;",
+//         [firstname, lastname, email, pasword_hash]
+//     );
+
+// };
+
+exports.saveUser = (firstname, lastname, email, password_hash) => {
     return db.query(
-        'INSERT INTO users (firstname, lastname, email, pasword_hash) VALUES($1, $2, $3, $4) RETURNING *;',
-        [firstname, lastname, email, pasword_hash]
+        "INSERT INTO users (firstname, lastname, email, password_hash) VALUES($1, $2, $3, $4) RETURNING *;",
+        [firstname, lastname, email, password_hash]
+    );
+};
+
+//----------------------------------------------------------------------------------
+exports.updateUser = (userID, firstname, lastname, email) => {
+    return db.query(
+        `UPDATE users SET firstname=$1, lastname=$2, email=$3 WHERE id=$4;`,
+        // 'UPDATE users SET (firstname, lastname, email, ) WHERE id = user_id VALUES($1, $2, $3, $4) RETURNING *; ', 
+        [firstname, lastname, email,userID]
+
     );
 
 };
+exports.updatePasswordHash = (userID, paswordHash) => {
+    return db.query(
+        //'UPDATE users SET (pasword_hash) WHERE id = user_id VALUES($1, $2) RETURNING *;',
+        `UPDATE users SET pasword_hash=$1, WHERE id=$2;`, 
+        [paswordHash, userID]
 
+    );
+};
+
+exports.updateOrInsertUserProfile = (userID, age, city, homepage) => {
+    return db.query(//hier normale ""?
+        `INSERT INTO profiles (user_id, age, city, homepage) VALUES($1, $2, $3, $4) ON CONFLICT (user_id) DO UPDATE SET age=$2, city=$3, homepage=$4;`,
+        [userID, age, city, homepage]
+
+    );
+};
+
+exports.deleteSignatureForUserId = userID => {
+    return db.query(
+        'DELETE FROM signatures WHERE user_id=$1;', [userID]
+
+    );
+};
+
+
+//----------------------------------------------------------------------------------
 exports.saveProfile =(userID, age, city, homepage) => {
     return db.query(
         'INSERT INTO profiles (user_id, age, city, homepage) VALUES($1, $2, $3, $4) RETURNING *;',
         [userID, age, city, homepage]   
+        
+        );
+    }; 
+    
+    exports.getUserByEmail = (email) => {
+        return db.query
+        ('SELECT * FROM users WHERE email = $1;', 
+        [email]
+        );
+    };
+    
+    exports.getSignatureByID = (signatureID) => {
+        return db.query(
+            'SELECT * FROM signatures WHERE id = $1;', 
+            [signatureID]
+        );    
+    };
+    
+    exports.getSignatureByUserID = (userID) => {
+        return db.query
+        ('SELECT * FROM signatures WHERE id = $1;', 
+        [userID]
+        );
+    };
+    exports.getAllUserInfoByUserID = () => {
+        return db.query(
+            'SELECT * FROM users LEFT JOIN profiles ON (users.id = profiles.user_id) WHERE (users.id = $1);', 
+            [userID]
+            );
+    };
 
-    );
-}; 
-
-exports.getUserByEmail = (email) => {
-    return db.query
-    ('SELECT * FROM users WHERE email = $1;', [email]);
-};
-
-exports.getSignatureByID = (signatureID) => {
-    return db.query
-    ('SELECT * FROM signatures WHERE id = $1;', [signatureID]);    
-};
-
-exports.getSignatureByUserID = (userID) => {
-    return db.query
-    ('SELECT * FROM signatures WHERE id = $1;', [userID])
-};
-exports.getSigners = () => {
-    return db.query
-    ('SELECT firstname, lastname FROM signatures JOIN users ON (signatures.id = users.id)' );
-};
+    exports.getSigners = () => {
+    return db.query(
+        'SELECT firstname, lastname, age, city, homepage FROM signatures JOIN users ON (signatures.user.id = users.id) JOIN profiles ON (users.id = profiles.user_id);', 
+        );
+    };
--- index.js
+++ index.js
@@ -17,9 +17,11 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('static'));
 app.use(require('body-parser')());
+
+//COOKIE SESSION
 app.use (cookieSession({
 secret: "Life is a big enigma",
-maxAge: 1000 * 60 * 60 * 24 * 120 //120 Tage 
+maxAge: 1000 * 60 * 60 * 24 * 69 //69 Tage 
 })
 );
 
@@ -57,7 +59,8 @@ app.post('/login', (request, response) => {
 
         });
     }; 
-    db.getUserByEmail(email).then((result) => {
+    db.getUserByEmail(email)
+    .then((result) => {
         const userPasswordHashFromDB = result.rows[0].password_hash;
         hashing.compare(password, userPasswordHashFromDB).then((isCorrect) => {
             if (isCorrect) {
@@ -70,7 +73,8 @@ app.post('/login', (request, response) => {
             }
         });
     }).catch(error => {
-        response.status(401).send('Your email or passwort are incorrect')
+        response.status(401).send('Your email or passwort are incorrect');
+        console.error(error);
     });
 
 });  
@@ -105,17 +109,15 @@ app.post('/register', (request, response)=>{
     } 
 
     
-
+//----
     //hash password, save user in db
-    hashing.generateHash(password).
-        then((password_hash)=>{
-        db.saveUser(firstname, lastname, email, password_hash)
-            .then((result) =>{
+    hashing.generateHash(password).then((password_hash)=>{
+        db.saveUser(firstname, lastname, email, password_hash).then((result) =>{
                 request.session.userID = result.rows[0].id;
                 response.redirect('/profile', 302);
             });
     });
-  
+  //----
 
 });
 
@@ -134,39 +136,130 @@ app.post('/profile', (request, response) => {
 
   
      //check if fields are filled 
+  
+    const age = request.body.age;
+    const city = request.body.city;
+    const homepage = request.body.homepage;
 
-    const {age, city, homepage} = request.body;
+
     if (!age || !city || !homepage) {
         return response.render('register', {
             error:'Required field/s are missing',
-            age, 
-            city, 
-            homepage
+            age, city, homepage,
 
         });
     } 
     //save user to DB
-    const 
+    const userID = request.session.userID;
+    db.saveProfile(userID, age, city, homepage).then(result => {
+         //redirect to home / canvas 
+         response.redirect ('/', 302)
+
+    }) .catch(error => {
+        response.status(503);
+        response.send('My appologies! Something goes terribly wrong'); 
+        console.error(error);
+    });    
 
 });  
 
 
-//------------------------------------------------
-app.get('/profile-edit', (request, response)=>{
-    response.render('profile-edit'); 
+// PROFILE EDIT  ---
+app.get('/profile-edit', (request, response) => {
+    if (!request.session.userID){//If not logged in redirect to login
+        return response.redirect('/login', 302);
+    }  
+    
+    db.getAllUserInfoByUserID(request, session.userID).then((result) => {
+        const userInfo = result.rows[0];
+        response.render('profile-edit', userInfo);
+
+    })
+    .catch((error) => {
+        response.status(500).send('Sorry! We do not recognise you.')
+    });
+});
+
+app.post ('/profile-edit', (request, response)=> { 
+    if(!request.session.userID){
+        return response.redirect('/login', 302);
+
+    }
+    // validate users entered data 
+    const firstname = request.body.firstname;
+    const lastname = request.body.lastname;
+    const email = request.body.email;
+    const password = request.body.password;
+    const age = request.body.age;
+    const city = request.body.city;
+    const homepage = request.body.homepage;
+
+    console.log ('request.body', request.body);  
+
+    if (!firstname || !lastname || !email || !age) {
+        return response.render('profile-edit', {
+           error: 'Please complete your profile: Firstname, Lastname, Email, Age, City, Homepage',
+           firstname, lastname, email, age, city, homepage,
+
+        });
+
+    }
+    //update user information
+    const userUpdatePr = db.updateUser(
+        request.session.userID,
+        firstname, lastname, email
+    );
+    
+    //update password hash if necessary
+    let passwordUpdatePr;
+    if(password) {
+        passwordUpdatePr = hashing.generateHash(password)
+        .then(passwordHash =>{
+            return db.updatePasswordHash(request.session.userID, passwordHash);
+
+        });
+    }
+
+    //update or insert user profile
+    const upsertPr = db.updateOrInsertUserProfile( request.session.userID, age, city, homepage);
+
+    Promise.all([
+        userUpdatePr,
+        passwordUpdatePr,
+        upsertPr
+    ]).then(data => {
+        response.redirect('/thank-you', 302);
+
+    });
+  
 
 });
 
-
-//------------------------------------------------
 
 
 // SIGN FORM (with canvas)
         
-app.get('/', (request, response)=>{
+app.get('/', (request, response) => {
     //if userID not in request,session -> redirect to login
-    response.render('home');
+    if (!request.session.userID) {
+        return response.redirect('/login', 302);
+    }
+
+    //check if user is signed 
+    db.getSignatureByUserID(request.session.userID).then((result) =>{
+        if (result.rows.length > 0){
+            response.redirect('/thank-you', 302);
+
+        }else {
+            response.render('home')
+        }
+
+    });   
+
 });
+
+
+
 
 app.post('/sign-petition', (request, response) => {
        let signatureCode = request.body.signatureCode;
@@ -193,11 +286,35 @@ app.post('/sign-petition', (request, response) => {
 
     });
 
-    app.get('/thank-you', (request, response) => {
+    //unsign petition
+
+    app.post('/unsign-petition', (request, response) => {
+        //check if user is loged in 
+        if (!request.session.userID) {
+            response.redirect('/login', 302)
+
+        }else {
+            //delete signature and redirect to canvas page
+            db.deleteSignatureForUserId(request.session.userID)
+            .then(result => {
+                response.redirect('/', 302);
+            });
+
+
+        }
+    });
+
+app.get('/thank-you', (request, response) => {
+        //if user is not in request.session, redirect to /login
+        
+        
+
+        const userID = request.session.userID;
         const signatureID = request.session.signatureID;
         console.log ('request for session on thank you page', request.session);  
         
-        db.getSignatureByID(signatureID)
+        //db.getSignatureByID(signatureID)
+        db.getSignatureByID(userID)
             .then(result => {
             const firstname = result.rows[0].firstname;
             const lastname = result.rows[0].lastname;
@@ -229,3 +346,4 @@ app.post('/sign-petition', (request, response) => {
 
 });
 app.listen(process.env.PORT || 8080);
+//app.listen(8080);
